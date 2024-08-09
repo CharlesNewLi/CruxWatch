6f55dc91ab98ebf724cf3ef777c65374a38676d2
yl882 <yl882@students.waikato.ac.nz>
2024-08-09 20:45:23 +1200

    update

--- React/src/components/footer/Footer.tsx
+++ React/src/components/footer/Footer.tsx
@@ -4,7 +4,16 @@ import { Layout, Typography } from "antd";
 export const Footer: React.FC = () => {
     return (
       <Layout.Footer>
-        <Typography.Title level={3} style={{ textAlign: "center" }}>
+          <Typography.Title
+            level={3}
+            style={{
+              textAlign: "center",
+              height: "5px",
+              display: "flex",
+              alignItems: "center", // 垂直居中
+              justifyContent: "center", // 水平居中
+            }}
+          >
           Copyright © CruxWatch
         </Typography.Title>
       </Layout.Footer>
